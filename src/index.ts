a227fa0d76ed931016d4f0f3ce8ff6c7d056fe40
SiDevesh <sid.swapnildevesh@gmail.com>
2020-07-14 02:42:45 +0530

    unprompt

--- src/index.ts
+++ src/index.ts
@@ -91,7 +91,3 @@ document.addEventListener('gesturestart', (e) => {
 });
 
 OfflinePluginRuntime.install();
-window.addEventListener('beforeinstallprompt', (e: any) => {
-    e.preventDefault();
-    e.prompt();
-});
